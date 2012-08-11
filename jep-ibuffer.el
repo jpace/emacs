c6c911f832d74cd2497b290aa669157b5fb6c608
Jeff Pace <jeugenepace@gmail.com>
2012-08-11 09:30:34 -0400

    Customized ibuffer to display directories with their projects (instead of in a separate group). Changed ibuffer to go to the most recent buffer.

--- jep-ibuffer.el
+++ jep-ibuffer.el
@@ -4,7 +4,7 @@
 (require 'ibuffer) 
 (setq ibuffer-saved-filter-groups
       (quote (("default"      
-	       ("dired" (mode . dired-mode))
+	       ;; ("dired" (mode . dired-mode))
 	       ("Org" ;; all org-related buffers
 		(mode . org-mode))  
 	       ;; ("Mail"
@@ -45,3 +45,20 @@
   (lambda ()
     (ibuffer-switch-to-saved-filter-groups "default")))
 
+;; Enable ibuffer-filter-by-filename to filter on directory names too.
+(eval-after-load "ibuf-ext"
+  '(define-ibuffer-filter filename
+     "Toggle current view to buffers with file or directory name matching QUALIFIER."
+     (:description "filename"
+		   :reader (read-from-minibuffer "Filter by file/directory name (regexp): "))
+     (ibuffer-awhen (or (buffer-local-value 'buffer-file-name buf)
+			(buffer-local-value 'dired-directory buf))
+		    (string-match qualifier it))))
+
+;; Switching to ibuffer puts the cursor on the most recent buffer
+(defadvice ibuffer (around ibuffer-point-to-most-recent) ()
+  "Open ibuffer with cursor pointed to most recent buffer name"
+  (let ((recent-buffer-name (buffer-name)))
+    ad-do-it
+    (ibuffer-jump-to-buffer recent-buffer-name)))
+(ad-activate 'ibuffer)
